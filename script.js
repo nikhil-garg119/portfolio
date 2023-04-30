2288759006c78960d4d908aae47619cecb0998d9
DratBoi <96489024+nikhil-garg119@users.noreply.github.com>
2023-05-01 02:59:53 +0530

    contact, footer section done

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky")
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
     });
 
     // toggle menu/navbar
@@ -12,6 +22,20 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Game Designer", "Level Designer", "Systems Designer", "Narrative Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+    var typed = new Typed(".typing-2", {
+        strings: ["Game Designer", "Level Designer", "Systems Designer", "Narrative Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
 
     //owl carousel script
     $('.carousel').owlCarousel({
